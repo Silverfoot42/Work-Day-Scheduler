e9675e0bd68765f0728f00966c1010856d9e1993
Silverfoot42 <elliottgc@gmail.com>
2023-09-25 01:16:59 -0400

    added the function to save the text to local storage

--- assets/script.js
+++ assets/script.js
@@ -1,141 +1,132 @@
-var hour9 = $('#hour-9');
-var hour10 = $('#hour-10');
-var hour11 = $('#hour-11');
-var hour12 = $('#hour-12');
-var hour13 = $('#hour-13');
-var hour14 = $('#hour-14');
-var hour15 = $('#hour-15');
-var hour16 = $('#hour-16');
-var hour17 = $('#hour-17');
+var hourX = [$('#hour-9'), $('#hour-10'), $('#hour-11'), $('#hour-12'), $('#hour-13'), $('#hour-14'), $('#hour-15'), $('#hour-16'), $('#hour-17')]
 var saveBTN = $('.saveBtn');
-hour9 = 9;
-hour10 = 10;
-hour11 = 11;
-hour12 = 12;
-hour13 = 13;
-hour14 = 14;
-hour15 = 15;
-hour16 = 16;
-hour17 = 17;
+var timeBlockId
+hourX[0] = 9;
+hourX[1] = 10;
+hourX[2] = 11;
+hourX[3] = 12;
+hourX[4] = 13;
+hourX[5] = 14;
+hourX[6] = 15;
+hourX[7] = 16;
+hourX[8] = 17;
 
 $(window).on("load", function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-
+    
     saveBTN.on('click', function () {
-      var scheduleInput = document.getElementById
-      localStorage.setItem("hour-9", scheduleInput.val());
+      timeBlockId = $(this).closest(".time-block").attr("id");
+      var userInput = $(this).siblings(".description").val();
+      localStorage.setItem(timeBlockId, userInput);
     });
     
     var currentHour = dayjs().hour();
-    if (currentHour < hour9) {
+    if (currentHour < hourX[0]) {
       $("#hour-9").addClass("future");
-      console.log(currentHour < hour9);
-    } else if (currentHour > hour9) {
+      console.log(currentHour < hourX[0]);
+    } else if (currentHour > hourX[0]) {
       $("#hour-9").addClass("past");
-      console.log(currentHour > hour9);
+      console.log(currentHour > hourX[0]);
     } else {
       $("#hour-9").addClass("present");
-      console.log(currentHour === hour9);
+      console.log(currentHour === hourX[0]);
     }
 
-    if (currentHour < hour10) {
+    if (currentHour < hourX[1]) {
       $("#hour-10").addClass("future");
-      console.log(currentHour < hour10);
-    } else if (currentHour > hour10) {
+      console.log(currentHour < hourX[1]);
+    } else if (currentHour > hourX[1]) {
       $("#hour-10").addClass("past");
-      console.log(currentHour > hour10);
+      console.log(currentHour > hourX[1]);
     } else {
       $("#hour-10").addClass("present");
-      console.log(currentHour === hour10);
+      console.log(currentHour === hourX[1]);
     }
 
-    if (currentHour < hour11) {
+    if (currentHour < hourX[2]) {
       $("#hour-11").addClass("future");
-      console.log(currentHour < hour11);
-    } else if (currentHour > hour11) {
+      console.log(currentHour < hourX[2]);
+    } else if (currentHour > hourX[2]) {
       $("#hour-11").addClass("past");
-      console.log(currentHour > hour11);
+      console.log(currentHour > hourX[2]);
     } else {
       $("#hour-11").addClass("present");
-      console.log(currentHour === hour11);
+      console.log(currentHour === hourX[2]);
     }
 
-    if (currentHour < hour12) {
+    if (currentHour < hourX[3]) {
       $("#hour-12").addClass("future");
-      console.log(currentHour < hour12);
-    } else if (currentHour > hour12) {
+      console.log(currentHour < hourX[3]);
+    } else if (currentHour > hourX[3]) {
       $("#hour-12").addClass("past");
-      console.log(currentHour > hour12);
+      console.log(currentHour > hourX[3]);
     } else {
       $("#hour-12").addClass("present");
-      console.log(currentHour === hour12);
+      console.log(currentHour === hourX[3]);
     }
 
-    if (currentHour < hour13) {
+    if (currentHour < hourX[4]) {
       $("#hour-13").addClass("future");
-      console.log(currentHour < hour13);
-    } else if (currentHour > hour13) {
+      console.log(currentHour < hourX[4]);
+    } else if (currentHour > hourX[4]) {
       $("#hour-13").addClass("past");
-      console.log(currentHour > hour13);
+      console.log(currentHour > hourX[4]);
     } else {
       $("#hour-13").addClass("present");
-      console.log(currentHour === hour13);
+      console.log(currentHour === hourX[4]);
     }
 
-    if (currentHour < hour14) {
+    if (currentHour < hourX[5]) {
       $("#hour-14").addClass("future");
-      console.log(currentHour < hour14);
-    } else if (currentHour > hour14) {
+      console.log(currentHour < hourX[5]);
+    } else if (currentHour > hourX[5]) {
       $("#hour-14").addClass("past");
-      console.log(currentHour > hour14);
+      console.log(currentHour > hourX[5]);
     } else {
       $("#hour-14").addClass("present");
-      console.log(currentHour === hour14);
+      console.log(currentHour === hourX[5]);
     }
 
-    if (currentHour < hour15) {
+    if (currentHour < hourX[6]) {
       $("#hour-15").addClass("future");
-      console.log(currentHour < hour15);
-    } else if (currentHour > hour15) {
+      console.log(currentHour < hourX[6]);
+    } else if (currentHour > hourX[6]) {
       $("#hour-15").addClass("past");
-      console.log(currentHour > hour15);
+      console.log(currentHour > hourX[6]);
     } else {
       $("#hour-15").addClass("present");
-      console.log(currentHour === hour15);
+      console.log(currentHour === hourX[6]);
     }
 
-    if (currentHour < hour16) {
+    if (currentHour < hourX[7]) {
       $("#hour-16").addClass("future");
-      console.log(currentHour < hour16);
-    } else if (currentHour > hour16) {
+      console.log(currentHour < hourX[7]);
+    } else if (currentHour > hourX[7]) {
       $("#hour-16").addClass("past");
-      console.log(currentHour > hour16);
+      console.log(currentHour > hourX[7]);
     } else {
       $("#hour-16").addClass("present");
-      console.log(currentHour === hour16);
+      console.log(currentHour === hourX[7]);
     }
 
-    if (currentHour < hour17) {
+    if (currentHour < hourX[8]) {
       $("#hour-17").addClass("future");
-      console.log(currentHour < hour17);
-    } else if (currentHour > hour17) {
+      console.log(currentHour < hourX[8]);
+    } else if (currentHour > hourX[8]) {
       $("#hour-17").addClass("past");
-      console.log(currentHour > hour17);
+      console.log(currentHour > hourX[8]);
     } else {
       $("#hour-17").addClass("present");
-      console.log(currentHour === hour17);
+      console.log(currentHour === hourX[8]);
     }
-    
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
 
-    localStorage.getItem("description");
+    $(".time-block").each(function() {
+      var timeID = $(this).attr("id");
+      var savedIn = localStorage.getItem(timeID);
+
+      if(savedIn) {
+        $(this).find(".description").val(savedIn);
+      }
+    });
 
     var today = dayjs();
     $('#currentDay').text(today.format('dddd, MMMM D'));
